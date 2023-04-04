60e8649907ad38ee103dd4f28fe80ab23b1f3b28
HYHYJ <jjangdaram@naver.com>
2023-04-04 14:04:29 +0900

    1

---
+++ app.js
@@ -0,0 +1,6 @@
+const title = document.getElementsByTagName("h1"); 
+// tag는 anchor, div, section, button 같은것을 의미
+
+const title = document.querySelector(".hello h1");
+//querySelector은 element를 CSS 방식으로 검색할 수 있다.
+ //ex) hello란 class 내부에 있는 h1을 가지고 올수 있다는 것을 의미
