62ca06bd527685b5a04a9837d296647ee239a5d1
HYHYJ <jjangdaram@naver.com>
2023-04-06 22:24:32 +0900

    23.04.06-2

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() { // 이 함수를 매 2초마다 실행
-    console.log("hello");
-}
+function getClock() { // 이 함수를 매 2초마다 실행
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
 
-setInterval(sayHello, 5000); //(무엇을 반복할껀지, 몇초) 지정한 초마다 업데이트
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+getClock()
+setInterval(getClock, 1000); //(무엇을 늦게 보이게 할껀지, 몇초) 지정한 초뒤에 보임
